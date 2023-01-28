e69058add3790c42c6ad56ae07f2aaf568fbd141
Rustam Khashimov <gnatangaro@gmail.com>
2023-01-29 01:48:54 +0300

    03_sum_and_dif

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+def sum_cyph(n):
+    summ = 0
+    while n > 0:
+        summ += n % 10
+        n //= 10
+    return summ
+
+def count_cyph(n):
+    count = 0
+    while n > 0:
+        count += 1
+        n //= 10
+    return count
+
+num = int(input('Введите число: '))
+x = sum_cyph(num)
+y = count_cyph(num)
+print(f'Сумма цифр: {x}')
+print(f'Количество цифр: {y}')
+print(f'Разность суммы и количества цифр: {x-y}')

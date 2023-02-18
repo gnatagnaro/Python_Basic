9a13dac9e6038e82563687244a49e85c21061a05
Rustam Khashimov <gnatangaro@gmail.com>
2023-02-18 03:30:06 +0300

    Module20

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def tpl_sort(*my_tuple):
+    if not int() in my_tuple:
+        return tuple(my_tuple)
+    return tuple(sorted(my_tuple))
+
+
+# print(tpl_sort(6, 3, -1, 8, 4, 10, -5, 'ff', [1, 2, 3]))
+# Ответ в консоли: (-5, -1, 3, 4, 6, 8, 10)
